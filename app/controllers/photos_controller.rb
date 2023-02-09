47701b548ee33df993ede591e727ba212446724e
lvmadoreira <lvaladao@chicagobooth.edu>
2023-02-09 06:46:58 +0000

    Photo Update Done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -26,4 +26,13 @@ class PhotosController < ApplicationController
 
     redirect_to("/photos/" + @new_photo.id.to_s)
   end
+
+  def update
+    @photo = Photo.where({ :id => params.fetch("photo_id") }).at(0)
+    @photo.image = params.fetch("input_image")
+    @photo.caption = params.fetch("input_caption")
+    @photo.save
+
+    redirect_to("/photos/" + @photo.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get("/photos/:photo_id", { :controller => "photos", :action => "details" })
   get("/delete_photo/:photo_id", { :controller => "photos", :action => "delete" })
   get("/insert_photo_record", { :controller => "photos", :action => "create" })
+  get("/update_photo/:photo_id", { :controller => "photos", :action => "update" })
 end
